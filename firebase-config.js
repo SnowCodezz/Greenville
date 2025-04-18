18bc33a9d5b956367fcfeba9d9b3f7e7634eeccd
SnowCodezz <genera-nova0j@icloud.com>
2025-04-17 21:22:04 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,35 @@
+// Save this as firebase-config.js
+(function(){
+    // Heavily obfuscated Firebase configuration
+    var _0x4a82=['charCodeAt','map','fromCharCode','join','split'];
+    
+    function _0xd3c4(_0x4e08b1,_0x1f5e95){
+        return String[_0x4a82[2]](_0x4e08b1-_0x1f5e95);
+    }
+    
+    function _0xf98c2(_0x3ae74f){
+        return _0x3ae74f[_0x4a82[4]]('')[_0x4a82[1]](function(_0x23f50a){
+            return _0x23f50a[_0x4a82[0]](0);
+        })[_0x4a82[1]](function(_0x1ce5d1){
+            return _0xd3c4(_0x1ce5d1,3);
+        })[_0x4a82[3]]('');
+    }
+    
+    var _0xenc1 = "DL}dv|FNS\\|Qht?UYul8YLj9XvGLrQheJDl2';";
+    var _0xenc2 = "gdwdedvh;0g6g<g1iluhedvhdss1frp";
+    var _0xenc3 = "gdwdedvh;0g6g<g";
+    var _0xenc4 = "gdwdedvh;0g6g<g1iluhedvhvwrudjh1dss";
+    var _0xenc5 = "z;3386962<z53";
+    var _0xenc6 = "4=z;3386962<z53=zhe=z377445i6587<88;g";
+    
+    window.initFirebase = function() {
+        return {
+            apiKey: _0xf98c2(_0xenc1),
+            authDomain: _0xf98c2(_0xenc2),
+            projectId: _0xf98c2(_0xenc3),
+            storageBucket: _0xf98c2(_0xenc4),
+            messagingSenderId: _0xf98c2(_0xenc5),
+            appId: _0xf98c2(_0xenc6)
+        };
+    };
+})();
