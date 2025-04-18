e97175a91262c1a22166eeb38399185e472d6f7a
SnowCodezz <genera-nova0j@icloud.com>
2025-04-17 21:28:07 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,26 +1,14 @@
 // Save this as firebase-config.js
 (function(){
-    // More reliable obfuscation that preserves the exact API key
-    function decode(str) {
-        return atob(str);
-    }
-    
-    // Base64 encoded credentials
-    var _0xb64a = "QUl6YVN5Q1BLeVduWGVxVlJWcmlVM3FCa05KSV9lYklDem55ektJ"; // apiKey
-    var _0xb64b = "ZGF0YWJhc2U4LWQzZDlkLmZpcmViYXNlYXBwLmNvbQ=="; // authDomain
-    var _0xb64c = "ZGF0YWJhc2U4LWQzZDlk"; // projectId
-    var _0xb64d = "ZGF0YWJhc2U4LWQzZDlkLmZpcmViYXNlc3RvcmFnZS5hcHA="; // storageBucket
-    var _0xb64e = "NzgwMDUzNjI5NzIw"; // messagingSenderId
-    var _0xb64f = "MTo3ODAwNTM2Mjk3MjA6d2ViOjcwNDQxMTJmMzI0OTU1ODI2MDZjNmQ="; // appId
-    
+    // Simple obfuscation that just moves the config out of the HTML
     window.initFirebase = function() {
         return {
-            apiKey: decode(_0xb64a),
-            authDomain: decode(_0xb64b),
-            projectId: decode(_0xb64c),
-            storageBucket: decode(_0xb64d),
-            messagingSenderId: decode(_0xb64e),
-            appId: decode(_0xb64f)
+            apiKey: "AIzaSyCPKyWnXeqVRVriU3qBkNJI_ebICznyzKI",
+            authDomain: "database8-d3d9d.firebaseapp.com",
+            projectId: "database8-d3d9d",
+            storageBucket: "database8-d3d9d.firebasestorage.app",
+            messagingSenderId: "780053629720",
+            appId: "1:780053629720:web:7044112f32495582606c6d"
         };
     };
 })();
