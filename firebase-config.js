2f295e5c90f7fc52c70d50c3f3ba9d7eeec743bb
SnowCodezz <genera-nova0j@icloud.com>
2025-04-17 21:32:03 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 // Save this as firebase-config.js
-(function(){
-    // Simple obfuscation that just moves the config out of the HTML
-    window.initFirebase = function() {
-        return {
-            apiKey: "AIzaSyCPKyWnXeqVRVriU3qBkNJI_ebICznyzKI",
-            authDomain: "database8-d3d9d.firebaseapp.com",
-            projectId: "database8-d3d9d",
-            storageBucket: "database8-d3d9d.firebasestorage.app",
-            messagingSenderId: "780053629720",
-            appId: "1:780053629720:web:7044112f32495582606c6d"
-        };
-    };
-})();
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCPKyWnXeqVRVriU3qBkNJI_ebICznyzKI",
+  authDomain: "database8-d3d9d.firebaseapp.com",
+  projectId: "database8-d3d9d",
+  storageBucket: "database8-d3d9d.firebasestorage.app",
+  messagingSenderId: "780053629720",
+  appId: "1:780053629720:web:7044112f32495582606c6d"
+};
+
+// Make it available to the window object so it can be accessed from your HTML
+window.firebaseConfig = firebaseConfig;
